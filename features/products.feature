218d66f4e44fe30df96f52afa66a282af3c88c28
zosh237 <tatchoubrice6@gmail.com>
2025-01-06 22:46:25 -0500

    BDD en bonne voie

--- features/products.feature
+++ features/products.feature
@@ -101,6 +101,7 @@ Scenario: List all products
     And I should see "Shoes" in the results
     And I should see "Big Mac" in the results
     And I should see "Sheets" in the results
+
 Scenario: Search by category
     When I visit the "Home Page"
     And I press the "Clear" button
@@ -139,12 +140,5 @@ Scenario: Search by available
     Then I should see the message "Success"
     And I should see "Hat" in the results
     And I should see "Big Mac" in the results
-And I should see "Sheets" in the results
+    And I should see "Sheets" in the results
     And I should not see "Shoes" in the results
-    Scenario: Search by name
-    When I visit the "Home Page"
-    And I set the "Name" to "Hat"
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the "Name" field
-    And I should see "A red fedora" in the "Description" field
--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -38,7 +38,7 @@ def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
     # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
+    context.driver.save_screenshot('home_page.png')
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
@@ -139,13 +139,14 @@ def step_impl(context, element_name, text_string):
 @when('I press the "{button}" button')
 def step_impl(context, button):
     button_id = button.lower() + '-btn'
-    context.driver.find_element_by_id(By.ID, button_id).click()
+    context.driver.find_element_by_id(button_id).click()
 
 @then(u'I should see "{name}" in the results')
 def step_impl(context, name):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
         expected_conditions.text_to_be_present_in_element(
             (By.ID, 'search_results'),
+            name
         )
     )
     assert(found)
@@ -156,14 +157,10 @@ def step_impl(context, name):
     assert(name not in element.text)
 
 
-
-@then(u'I should see the message "Success"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should see the message "Success"')
-
-
-
-
+@then(u'I should see the message "{message_text}"')
+def step_impl(context, message_text):
+    element = context.driver.find_element_by_id('flash_message')
+    assert(message_text in element.text)
 
 
 
@@ -197,18 +194,4 @@ def step_impl(context):
 def step_impl(context):
     raise NotImplementedError(u'STEP: When I click the "Retrieve" button')
 
-
-       
-        )
-    )
-
-
-
-
-
-
-
-@then(u'I should see the message "Product has been Deleted!"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should see the message "Product has been Deleted!"')
 
